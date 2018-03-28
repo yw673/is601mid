2c404bfe2f899afcc4cd41ea8cdcb8ee23bcf5e0
yangyin <yw673@njit.edu>
2018-03-28 11:07:32 -0400

    Test Contact page return a 200 status code

---
+++ tests/Feature/ContactTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ContactTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testContactPage()
+    {
+        $response = $this->get('/contact');
+
+        $response->assertStatus(200);
+    }
+}
