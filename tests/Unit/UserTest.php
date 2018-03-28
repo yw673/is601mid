e668141e8be3c71838fa51e41ef1d1862f49f572
yangyin <yw673@njit.edu>
2018-03-28 11:29:20 -0400

    Test to insert a user into users table

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = new User();
+        $user->name = 'my name';
+        $user->email = 'my email';
+        $user->password = 'mypassword';
+
+        $this->assertTrue($user->save());
+    }
+}
