868e96a5715b3c08272142b7e3769606429ce74f
yangyin <yw673@njit.edu>
2018-03-28 14:18:07 -0400

    Create test to insert a new car data

---
+++ tests/Unit/CarTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class CarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $car = new Car();
+        $car->make = 'my make';
+        $car->model = 'my mode';
+        $car->year = 'my year';
+
+        $this->assertTrue($car->save());
+
+        $car->delete();
+    }
+}
